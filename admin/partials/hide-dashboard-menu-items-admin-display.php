7aa91cebe83d7189ec414bb1de4c9e1807bc2188
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2025-07-09 15:25:27 +0530

    Add notice to admin settings view for first time scan

--- admin/partials/hide-dashboard-menu-items-admin-display.php
+++ admin/partials/hide-dashboard-menu-items-admin-display.php
@@ -10,3 +10,13 @@
  * @package    Hide_Dashboard_Menu_Items
  * @subpackage Hide_Dashboard_Menu_Items/admin/partials
  */
+if (isset($_GET['hdmi_scan_success'])) {
+    add_settings_error(
+        'hdmi_scan_notice',
+        'hdmi_scan_success',
+        'Initial admin menu scan completed successfully!',
+        'success'
+    );
+}
+
+settings_errors('hdmi_scan_notice');
