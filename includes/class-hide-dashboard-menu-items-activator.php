d5d0a30f3d3d5ac143f9ab218895e2055640d3ed
Danuka Prasad <121777864+danuabd@users.noreply.github.com>
2025-07-12 09:31:09 +0530

    Add plugin activation prevention conditions

--- includes/class-hide-dashboard-menu-items-activator.php
+++ includes/class-hide-dashboard-menu-items-activator.php
@@ -16,5 +16,26 @@ class Hide_Dashboard_Menu_Items_Activator
 	/**
 	 * @since    1.0.0
 	 */
-	public static function activate() {}
+	public static function activate()
+	{
+		if (version_compare(PHP_VERSION, '7.4', '<')) {
+			wp_die(
+				esc_html__('Hide Dashboard Menu Items requires PHP version 7.4 or higher.', 'hide-dashboard-menu-items')
+			);
+		}
+
+		// Check if the current user has the capability to activate plugins
+		if (! current_user_can('activate_plugins')) {
+			wp_die(
+				esc_html__('You do not have sufficient permissions to activate this plugin.', 'hide-dashboard-menu-items')
+			);
+		}
+
+		// Check if the plugin is already activated
+		if (is_plugin_active('hide-dashboard-menu-items/hide-dashboard-menu-items.php')) {
+			wp_die(
+				esc_html__('The Hide Dashboard Menu Items plugin is already activated.', 'hide-dashboard-menu-items')
+			);
+		}
+	}
 }
